446c0833495dacafde32c4843a5132dc33bfa99c
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-11-11 11:17:41 +0800

    bump version

--- setup.py
+++ setup.py
@@ -1,42 +1,42 @@
-from setuptools import setup, find_packages
-import sys
 import os
+import sys
 
-version = '0.1.2'
+from setuptools import find_packages, setup
 
-setup(name='rulez',
-      version=version,
-      description="Simple business rules engine configurable using YAML/JSON",
-      long_description="""\
+version = "0.1.3"
+
+setup(
+    name="rulez",
+    version=version,
+    description="Simple business rules engine configurable using YAML/JSON",
+    long_description="""\
 """,
-      # Get strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-
-      classifiers=[],
-
-      keywords='business-rules rules reg',
-      author='Izhar Firdaus',
-      author_email='izhar@abyres.net',
-      url='http://github.com/abyres/',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'reg',
-          'dectate',
-          'boolean.py',
-          'jsonpath_ng',
-          'sqlalchemy'
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={
-          'test': [
-              'mirakuru',
-              'elasticsearch>=5.0.0,<6.0.0',
-          ]
-      },
-      entry_points="""
+    # Get strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
+    keywords="business-rules rules reg",
+    author="Izhar Firdaus",
+    author_email="izhar@abyres.net",
+    url="http://github.com/abyres/",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "reg",
+        "dectate",
+        "boolean.py",
+        "jsonpath_ng",
+        "sqlalchemy"
+        # -*- Extra requirements: -*-
+    ],
+    extras_require={
+        "test": [
+            "mirakuru",
+            "elasticsearch>=5.0.0,<6.0.0",
+        ]
+    },
+    entry_points="""
       # -*- Entry points: -*-
       """,
-      )
+)
